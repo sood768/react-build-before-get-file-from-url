154f3448288521ec45134ed754072502d1483545
sudarhan bairagi <sonubairagi9@gmail.com>
2020-10-22 23:03:43 +0530

    uploaded

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+class App extends Component {
+  state = {
+    data: { value1: 'https://www.happywithwords.in/', value2: "ReactJs Test" }
+  }
+  render() {
+    const { data } = this.state;
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return <ul>
+      {
+        Object.keys(data).map((value, index) => <li key={index}>{data[value]}</li>)
+      }
+    </ul>
+  }
 }
-
 export default App;
